0316ac0f81ab4eb6d8d170c81e871b8eb77950f3
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-02-19 23:02:12 +0200

    lesson one

---
+++ 1.compiling_typescript/sandbox.js
@@ -0,0 +1,6 @@
+var character = 'Ben';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ 1.compiling_typescript/sandbox.ts
@@ -0,0 +1,9 @@
+const character = 'Ben';
+console.log(character);
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    console.log(input)
+    
+});
